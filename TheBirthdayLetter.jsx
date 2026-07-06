287754efeb1fdb0f18d838956db464b8e4f22b1b
c4txlc <hoanglinhchimushroom@gmail.com>
2026-07-06 19:18:59 +0700

    TheBirthdayLetter.jsx

--- TheBirthdayLetter.jsx
+++ TheBirthdayLetter.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; 
 
+import thanksLetter from "./thanks-letter.png";
+import sorryLetter from "./sorry-letter.png";
+import happyLetter from "./happy-letter.png";
+import sadLetter from "./sad-letter.png";
+import letter2 from "./letter2.png";
+
 function TheBirthdayLetter() {
     const navigate = useNavigate();
     const [popupImage, setPopupImage] = useState(null);
@@ -18,19 +24,17 @@ function TheBirthdayLetter() {
             <div className="paper">
                 <div className="letter">
                     <p style={{ lineHeight: '1.5', marginBottom: '15px' }}>
-        Salut, joyeux anniversaire, mon cœur !!!
-      </p>
-      
-      <p style={{ lineHeight: '1.5', marginBottom: '15px' }}>
-        Tu viens de franchir un nouveau chapitre de ta vie ! Je sais que tu peux y arriver et je crois en toi. 
-        Tu l'as fait !!! S'il te plaît, reste en vie :D Il faut qu'on se retrouve et qu'on fête ton anniversaire 
-        ensemble, en personne hihi :DD
-      </p>
-      
-      <p style={{ lineHeight: '1.5' }}>
-        Encore une fois, je te souhaite de profiter pleinement de ce nouveau chapitre de ta vie, 
-        et j'espère que le bonheur t'accompagnera à chaque seconde.
-      </p>
+                        Salut, joyeux anniversaire, mon cœur !!!
+                    </p>
+                    <p style={{ lineHeight: '1.5', marginBottom: '15px' }}>
+                        Tu viens de franchir un nouveau chapitre de ta vie ! Je sais que tu peux y arriver et je crois en toi. 
+                        Tu l'as fait !!! S'il te plaît, reste en vie :D Il faut qu'on se retrouve et qu'on fête ton anniversaire 
+                        ensemble, en personne hihi :DD
+                    </p>
+                    <p style={{ lineHeight: '1.5' }}>
+                        Encore une fois, je te souhaite de profiter pleinement de ce nouveau chapitre de ta vie, 
+                        et j'espère que le bonheur t'accompagnera à chaque seconde.
+                    </p>
                     <p style={{ textAlign: "right" }}>bisous,</p>
                     <p style={{ textAlign: "right" }}>cat</p>
                 </div>
@@ -39,24 +43,25 @@ function TheBirthdayLetter() {
             <div className="title">
                 <p>ta réaction?</p>
             </div>
+
             <div className="img-btn">
-                <button className="img-button" onClick={() => openPopup("thanks-letter.png")}>
-                    <img src="letter2.png" alt="letter" className="img-small"/>
+                <button className="img-button" onClick={() => openPopup(thanksLetter)}>
+                    <img src={letter2} alt="letter" className="img-small"/>
                     <span className="btn-text">merci</span>
                 </button>
-                <button className="img-button" onClick={() => openPopup("sorry-letter.png")}>
-                    <img src="letter2.png" alt="letter" className="img-small"/>
+                <button className="img-button" onClick={() => openPopup(sorryLetter)}>
+                    <img src={letter2} alt="letter" className="img-small"/>
                     <span className="btn-text">désolé</span>
                 </button>
             </div>
 
             <div className="img-btn">
-                <button className="img-button" onClick={() => openPopup("happy-letter.png")}>
-                    <img src="letter2.png" alt="letter" className="img-small"/>
+                <button className="img-button" onClick={() => openPopup(happyLetter)}>
+                    <img src={letter2} alt="letter" className="img-small"/>
                     <span className="btn-text">heureux</span>
                 </button>
-                <button className="img-button" onClick={() => openPopup("sad-letter.png")}>
-                    <img src="letter2.png" alt="letter" className="img-small"/>
+                <button className="img-button" onClick={() => openPopup(sadLetter)}>
+                    <img src={letter2} alt="letter" className="img-small"/>
                     <span className="btn-text">triste</span>
                 </button>
             </div>
